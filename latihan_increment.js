065b23c85618e6a9f02c2859b6050df8cacc9f69
Julkahfi <andraalam26@gmail.com>
2022-09-28 13:37:32 +0700

    Chapter_1_270922

---
+++ latihan_increment.js
@@ -0,0 +1,27 @@
+console.log('===increment===')
+var a = 0
+var b = 10
+var a = b+=5
+console.log(a)
+console.log(b)
+
+var c = 0
+var d = 10
+var c = d+=5
+console.log(c)
+console.log(d)
+
+console.log('===decrement===')
+var e = 0
+var f = 10
+var e = f--
+var f = f-1
+console.log(e)
+console.log(f)
+
+var g = 0
+var h = 10
+var g = --h-1
+var h = g
+console.log(g)
+console.log(h)
---
+++ mathOperator_4_increment.js
@@ -0,0 +1,19 @@
+//increment
+var a = 0
+var b = 10
+var a = b++
+console.log(a)
+console.log(b)
+
+var c = 10
+var d = 8
+var c = ++d
+console.log(c)
+console.log(d)
+
+//decrement
+var e = 10 
+var f = 8
+var e = f--
+console.log(e)
+console.log(f)
