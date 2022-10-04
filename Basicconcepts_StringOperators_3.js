ad4bed6b0459da390c467e18b8ced1f46fc37bd6
Julkahfi <andraalam26@gmail.com>
2022-10-04 11:48:48 +0700

    3a_ConditionalsandLoops_041022

---
+++ Basicconcepts_StringOperators_3.js
@@ -0,0 +1,3 @@
+var mystring1 = "I am learning "
+var mystring2 = "JavaSript with Sololearn"
+console.log(mystring1 + mystring2)
---
+++ ConditionalsandLoops_elseif_2.js
@@ -0,0 +1,8 @@
+var course = 1;
+if (course == 1) {
+    console.log("<h1> HTML Tutorial</h1>")
+}else if (course == 2){
+    console.log("<h1> CSS Tutorial")
+}else {
+    console.log("<h1>JavaScript Tutorial</h1>")
+}
